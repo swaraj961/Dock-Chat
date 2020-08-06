9d4d2ca20616fbb063d9b5b55d7bc8a1c9039504
swaraj961 <swarajroutray961@gmail.com>
2020-08-07 04:12:57 +0530

    SearchUser Working now

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dockchat/Models/user.dart';
 import 'package:dockchat/Pages/Extra/create_account.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -9,7 +10,6 @@ import 'package:flutter/material.dart';
 import 'package:dockchat/Pages/ChattingPage.dart';
 import 'package:dockchat/main.dart';
 import 'package:dockchat/Pages/AccountSettingsPage.dart';
-import 'package:dockchat/models/user.dart';
 import 'package:dockchat/Widgets/ProgressWidget.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -19,12 +19,16 @@ class HomeScreen extends StatefulWidget {
 
   HomeScreen({Key key, @required this.currentUserID}) : super(key: key);
   @override
-  State createState() => HomeScreenState();
+  State createState() => HomeScreenState(currentUserID);
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  String currentuserId;
   GoogleSignIn _googleSignIn = GoogleSignIn();
   TextEditingController _textEditingController = TextEditingController();
+  Future<QuerySnapshot> futureSearchResults;
+
+  HomeScreenState(this.currentuserId);
 
   Future<Null> logoutUser() async {
     await FirebaseAuth.instance.signOut();
@@ -40,31 +44,28 @@ class HomeScreenState extends State<HomeScreen> {
     return AppBar(
       backgroundColor: Theme.of(context).primaryColor,
       title: Container(
-      
-      margin: EdgeInsets.only(bottom: 4),
+        margin: EdgeInsets.only(bottom: 4),
         child: TextFormField(
-          //  textAlign: TextAlign.center, 
+          //  textAlign: TextAlign.center,
           controller: _textEditingController,
           style: TextStyle(fontSize: 18.0, color: Colors.white),
           decoration: InputDecoration(
-             contentPadding: const EdgeInsets.only(bottom: 8),
+              contentPadding: const EdgeInsets.only(bottom: 8),
               hintText: 'Search for users',
               hintStyle: TextStyle(
-              
                 color: Colors.white,
               ),
-              
-            
               enabledBorder: UnderlineInputBorder(
                 borderSide: BorderSide(color: Colors.grey),
               ),
               focusedBorder: UnderlineInputBorder(
                 borderSide: BorderSide(color: Colors.white),
               ),
+              //  focusedBorder: InputBorder.none,
               prefixIcon: Icon(
                 Icons.person_pin,
                 color: Colors.white,
-                size:30,
+                size: 30,
               ),
               suffix: IconButton(
                 icon: Icon(
@@ -77,7 +78,9 @@ class HomeScreenState extends State<HomeScreen> {
                     _textEditingController.clear();
                   });
                 },
-              )),
+              ),
+              ),
+              onFieldSubmitted: implementSearch,
         ),
       ),
       automaticallyImplyLeading: false,
@@ -103,23 +106,107 @@ class HomeScreenState extends State<HomeScreen> {
     );
   }
 
+void implementSearch(String userName){
+
+Future<QuerySnapshot> allDockchatusers = Firestore.instance.collection('users').where('nickname', isGreaterThanOrEqualTo: userName).getDocuments();
+setState(() {
+  futureSearchResults = allDockchatusers;
+});
+
+ }
+
+displayNOsearchResultScreen(){
+  final Orientation orientation = MediaQuery.of(context).orientation;
+return Container(
+  child: Center(
+    child: ListView(
+      shrinkWrap: true,
+      children: <Widget>[
+ Icon(Icons.group, size: 200, color: Colors.white,),
+        Text('Search user', style: TextStyle(
+          fontWeight: FontWeight.w700 ,  color: Colors.white, fontSize: 50,
+        ),
+        textAlign: TextAlign.center,),
+
+      ],
+    ),
+  ),
+);
+
+}
+
+userFoundScreen(){
+
+  return FutureBuilder(
+   future: futureSearchResults,
+  
+   builder: (context,datasnapshot){ 
+     if(!datasnapshot.hasData){
+
+       return circularProgress();
+     } else {
+
+List <UserResult> searchResultList =[];
+datasnapshot.data.documents.forEach((document){
+
+User eachuser = User.fromDocument(document);
+UserResult userResult = UserResult(eachuser: eachuser,);
+
+if(currentuserId!=document['id']){
+
+//checking for same user can't search for himself 
+// showing users if  search user is not same as current user
+searchResultList.add(userResult);
+}
+});
+return ListView(
+  children: searchResultList,
+);
+     }
+   },
+  
+  
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff251F34),
       appBar: homePageHeader(),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Logout'),
-          onPressed:logoutUser),
-      ),
+      body:  futureSearchResults == null ? displayNOsearchResultScreen(): userFoundScreen(),
     );
+
+    
   }
 }
 
+
+
 class UserResult extends StatelessWidget {
+  final User eachuser;
+
+  const UserResult({Key key, this.eachuser}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.black,
+              backgroundImage: CachedNetworkImageProvider(eachuser.photoUrl),
+            ),
+            title: Text(eachuser.nickname, style: TextStyle(fontWeight: FontWeight.w600, color: Colors.grey),),
+            subtitle: Text("Joined DockChat at :"+ DateFormat("dd MMMM , yyyy - hh:mm:aa").format(DateTime.fromMillisecondsSinceEpoch(int.parse(eachuser.createdAt)))
+          , style: TextStyle(fontWeight: FontWeight.w600, color: Colors.grey),),
+          )
+        ],
+      )
+    ); 
   }
 }
